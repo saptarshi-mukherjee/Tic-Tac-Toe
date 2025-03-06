33451362ee67a45c45c06870732bb4a8df2b9af9
saptarshi <sapbum1234@gmail.com>
2025-03-06 20:11:20 +0530

    Factory minor change

--- src/Factories/WinningStrategyFactory.java
+++ src/Factories/WinningStrategyFactory.java
@@ -6,7 +6,7 @@ import Strategies.Winner.WinningStrategy;
 
 public class WinningStrategyFactory {
     public static WinningStrategy getStrategy(int player_size) {
-        if(player_size>1)
+        if(player_size>100)
             return new LargeBoardStrategy(player_size+1);
         else
             return new SmallBoardStrategy();
